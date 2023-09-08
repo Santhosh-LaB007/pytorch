1069433809e392866caf54d24a91a40f4bf9a7fc
Ying Zhang <yingz@meta.com>
2023-09-08 16:02:16 -0700

    CUDAcodecache
    ghstack-source-id: f06725443e56847f2ea27a3196ec82c23432ebd7
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/107847

--- torch/_inductor/utils.py
+++ torch/_inductor/utils.py
@@ -7,6 +7,7 @@ import logging
 import math
 import operator
 import os
+import platform
 import shutil
 import sys
 import tempfile
@@ -1039,6 +1040,10 @@ def is_welford_reduction(reduction_type):
 
 def reduction_num_outputs(reduction_type):
     return 3 if is_welford_reduction(reduction_type) else 1
+
+
+def is_linux() -> bool:
+    return platform.system() == "Linux"
 
 
 # A utility function for easier AOTInductor testing
